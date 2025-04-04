8b1bd1a68865fc1f6eb87015784544509151070e
Ald28 <aldopichihua123@gmail.com>
2025-04-04 00:16:31 -0500

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SqlConnection connection =
+            new SqlConnection("Data Source=DESKTOP-QMNREBJ\\SQLEXPRESS2017; Initial Catalog=LabDB; Integrated Security=True; TrustServerCertificate=True; Encrypt = True");
+
+                connection.Open();
+                DataTable dataTable = new DataTable();
+                SqlCommand command = new SqlCommand("Select * from Products", connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(dataTable);
+                connection.Close();
+
+                dgDemo1.ItemsSource = dataTable.DefaultView;
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.ToString());
+                throw;
+            }
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection =
+           new SqlConnection("Data Source=DESKTOP-QMNREBJ\\SQLEXPRESS2017; Initial Catalog=LabDB; Integrated Security=True; TrustServerCertificate=True; Encrypt = True");
+
+            List<Producto> products = new List<Producto>();
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("Select * from Products", connection);
+            SqlDataReader dataReader = command.ExecuteReader();
+            while (dataReader.Read()) {
+
+                products.Add(new Producto
+                {
+                    ProductID = Convert.ToInt32(dataReader["ProductID"].ToString()),
+                    Nombre = dataReader["Nombre"].ToString(),
+                    Precio = dataReader["Precio"].ToString(),
+                });
+
+            }
+            connection.Close();
+            dgDemo2.ItemsSource = products;
+        }
+    }
+}
---
+++ Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Producto
+    {
+        public int ProductID { get; set; }
+        public string Nombre { get; set; }
+        public string Precio { get; set; }
+    }
+}
